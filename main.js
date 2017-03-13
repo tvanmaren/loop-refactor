b8d8239188a58757d71336e77868d39db260c436
Tristan Van Maren <tm.vanmaren@gmail.com>
2017-03-13 12:35:09 -0600

    :sparkles: all tests passing

--- main.js
+++ main.js
@@ -13,49 +13,34 @@ Run the command 'mocha' to test. Tests check for expected output and absence of 
 module.exports = {
 
   sum: (arr, base) => {
-    let sum = base;
-    for (var i = 0; i < arr.length; i++){
-      sum += arr[i];
-    }
-    return sum;
+    return arr.reduce((sum, val) => {
+      return sum+val;
+    }, base);
   },
 
   someObjsContainProp: (arr, prop) => {
-    for(var i = 0; i < arr.length; i++){
-      if(arr[i].hasOwnProperty(prop)){
-        return true;
-      }
-    }
-    return false;
+    return arr.some((obj) => {
+      return (obj.hasOwnProperty(prop));
+    });
   },
 
   convertNameArrayToObject: (arr) => {
-    let nameObj = [];
-    for(var i = 0; i < arr.length; i++){
-      let obj = {};
-      obj.first = arr[i][0];
-      obj.last = arr[i][1];
-      nameObj.push(obj);
-    }
-    return nameObj;
+    return arr.map((subArr) => {
+      const first=subArr[0];
+      const last=subArr[1];
+      return { first, last };
+    });
   },
 
   objContainsProp: (arr, prop) => {
-    for (var i = 0; i < arr.length; i++){
-      if(!arr[i].hasOwnProperty(prop)){
-        return false;
-      }
-    }
-    return true;
+    return arr.every((obj) => {
+      return obj.hasOwnProperty(prop);
+    });
   },
 
   stringMatch: (arr, str) => {
-    let matches = [];
-    for(var i = 0; i < arr.length; i++){
-      if (arr[i].includes(str)){
-        matches.push(arr[i]);
-      }
-    }
-    return matches;
+    return arr.filter((el) => {
+      return el.includes(str);
+    });
   },
 };
